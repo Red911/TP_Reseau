41575fec06b8be2d726f8afc4af7f056fea03004
Romain <mromain911@gmail.com>
2025-01-15 22:06:01 -0500

    Initialize Unreal

---
+++ Source/TP_Reseau/TP_ReseauGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TP_ReseauGameMode.h"
+#include "TP_ReseauCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATP_ReseauGameMode::ATP_ReseauGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/TP_Reseau/TP_ReseauGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TP_ReseauGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATP_ReseauGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATP_ReseauGameMode();
+};
+
+
+
