f5b2864bf36826f14f9906e6700747121f05a528
Romain <mromain911@gmail.com>
2025-01-18 14:39:32 -0500

    GameInstance

---
+++ Source/TP_Reseau/NetworkGameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NetworkGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TP_RESEAU_API UNetworkGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
